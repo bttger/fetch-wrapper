dc3201d3b85bc2d07984607a0bfdd03e14df53e2
Tom Boettger <t.boettger@live.de>
2023-12-21 11:23:18 +0100

    Initialize project

--- lib.ts
+++ lib.ts
@@ -3,7 +3,10 @@ export type BeforeInterceptorFunc = (
   options: EnhancedRequestInit,
 ) => Promise<{ url: string; options: EnhancedRequestInit }>;
 
-export type AfterInterceptorFunc = (response: Response, json?: any) => Promise<any>;
+export type AfterInterceptorFunc = (
+  response: Response,
+  json?: Record<string, any>,
+) => Promise<Record<string, any>>;
 
 /**
  * Enhanced `Error` class for preserving the stack trace if another error has been thrown before
@@ -28,7 +31,7 @@ export class EnhancedFetchError extends Error {
  */
 export type EnhancedRequestInit = RequestInit & {
   /** The data object to be sent as JSON. */
-  data?: Record<string, any> | File;
+  data?: Record<string, any>;
   /**
    * The plain object to be serialized into query string. Params that are
    * null or undefined are not rendered in the URL. Array values of the
@@ -54,9 +57,7 @@ export type EnhancedRequestInit = RequestInit & {
 };
 
 /**
- * Simple Fetch API Wrapper.
- *
- * This wrapper enhances the native fetch API with the following features:
+ * Calls the native fetch API with the following enhanced features:
  * - Automatic query string serialization from a params object.
  * - Automatic JSON serialization if data object is provided.
  * - Automatic setting of Content-Type header to application/json if data object is provided.
@@ -65,7 +66,7 @@ export type EnhancedRequestInit = RequestInit & {
  * - Customizable response timeout.
  *
  * @param url The URL to request.
- * @param options The enhanced fetch options object.
+ * @param options The enhanced fetch options object. Supports code completion for all native fetch options.
  * @returns Returns a promise with the result of the fetch call (parsed JSON body) or throws an `EnhancedFetchError`.
  */
 export async function enhancedFetch(url: string, options?: EnhancedRequestInit): Promise<any> {
@@ -75,19 +76,11 @@ export async function enhancedFetch(url: string, options?: EnhancedRequestInit):
 
   // If data object is provided, serialize it as JSON
   if (options.data) {
-    if (options.data instanceof File) {
-      options.body = options.data;
-      options.headers = {
-        ...options.headers,
-        "Content-Type": "application/octet-stream",
-      };
-    } else {
-      options.body = JSON.stringify(options.data);
-      options.headers = {
-        ...options.headers,
-        "Content-Type": "application/json",
-      };
-    }
+    options.body = JSON.stringify(options.data);
+    options.headers = {
+      ...options.headers,
+      "Content-Type": "application/json",
+    };
   }
 
   // Serialize params into query string and attach to the URL
@@ -110,41 +103,44 @@ export async function enhancedFetch(url: string, options?: EnhancedRequestInit):
   options.signal = abortController.signal;
   const timeoutId = setTimeout(() => abortController.abort(), timeout);
 
+  // Execute the fetch call
   let response: Response;
   try {
     response = await fetch(url, options);
     clearTimeout(timeoutId);
-  } catch (err: any) {
-    // Network error happened
-    throw new EnhancedFetchError(err.message, undefined, undefined, err.stack);
+  } catch (err) {
+    // AbortError or TypeError (e.g. invalid fetch options or network error)
+    if ((err instanceof DOMException && err.name === "AbortError") || err instanceof TypeError) {
+      throw new EnhancedFetchError(err.message, undefined, undefined, err.stack);
+    }
+    throw err;
   }
 
   // Check if response status is within the range of 200-299
   if (response.ok) {
-    // Automatically parse response as JSON
+    // Parse JSON response to object
     if (response.headers.get("Content-Type")?.includes("application/json")) {
       const result = await response.json();
+
       // Execute the after interceptor if provided
       if (afterInterceptor && typeof afterInterceptor === "function") {
         return await afterInterceptor(response, result);
       }
+
       return result;
-    } else if (response.headers.get("Content-Type")?.includes("application/octet-stream")) {
-      const result = await response.blob();
-      console.log(response.headers.get("Content-Disposition"));
-      return {
-        fileName: response.headers.get("Content-Disposition")?.substring("attachment; filename=".length),
-        data: result,
-      };
     } else {
       throw new EnhancedFetchError("Response Content-Type is unsupported");
     }
   } else {
     // Execute the after interceptor if provided
     if (afterInterceptor && typeof afterInterceptor === "function") {
-      await afterInterceptor(response, null);
+      await afterInterceptor(response, {});
     }
-    throw new EnhancedFetchError("Response status out of 200-299 range", response.status, response.statusText);
+    throw new EnhancedFetchError(
+      "Response status out of 200-299 range",
+      response.status,
+      response.statusText,
+    );
   }
 }
 
@@ -156,8 +152,8 @@ export async function enhancedFetch(url: string, options?: EnhancedRequestInit):
 function serializeParams(params: Record<string, any>): string {
   const parts = [];
 
-  for (let [key, value] of Object.entries(params)) {
-    if (value === null || value === undefined) continue;
+  for (const [key, value] of Object.entries(params)) {
+    if (value == null) continue;
 
     if (Array.isArray(value)) {
       value.forEach((v) => parts.push(`${encodeURIComponent(key)}=${encodeURIComponent(v)}`));
